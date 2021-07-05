aaf56a8a2be4507519b8aed62730c8e87b265235
LadyAnith <LadyAnith@gmail.com>
2021-07-05 11:08:15 +0200

    Pruebas subida2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-package main;
+
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Holiiiii");
 		System.out.println("Chaooooooooooo");
+		System.out.println("Haciendo prueblas con Jenkins");	
+		System.out.println("Haciendo prueblas con Jenkins234643636");
 	}
 }
